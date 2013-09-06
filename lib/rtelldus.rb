22f994d606276d35ff117c56ff0952a3aab5b4a6
Eric Ripa <eric@ripa.io>
2013-09-06 19:51:08 +0200

    Added YAML configuration template and started to setup oauth

--- lib/rtelldus.rb
+++ lib/rtelldus.rb
@@ -1,5 +1,14 @@
 require "rtelldus/version"
+require 'oauth'
+require 'json'
+require 'yaml'
 
 module Rtelldus
-  # Your code goes here...
+  config = YAML.load('config.yaml')
+
+  oauth_options = config['telldus-api']
+
+  # Set up LinkedIn specific OAuth API endpoints
+
+  consumer = OAuth::Consumer.new(oauth_options['consumer_key'], oauth_options['consumer_secret'], consumer_options)
 end
