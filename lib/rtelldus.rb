b8ba6ec460663966b894c4cf2a8fe66400bae68c
Eric Ripa <eric@ripa.io>
2013-09-07 23:10:06 +0200

    Removed the need to have a configuration file, now the user will be asked for the keys on first launch and then it will be saved to ~/.rtelldus

--- lib/rtelldus.rb
+++ lib/rtelldus.rb
@@ -1,20 +1,36 @@
 require "rtelldus/version"
 require 'oauth'
 require 'json'
-require 'yaml'
 require 'user_config'
 
 
 module Rtelldus
-  @config = YAML.load_file 'lib/config.yaml'
   @uconf = UserConfig.new(".rtelldus")
   def self.authorize
-    oauth_options = @config['telldus-api']
-    consumer_options = { :site => oauth_options['api_host'],
-                         :authorize_path => oauth_options['authorize_path'],
-                         :request_token_path => oauth_options['request_token_path'],
-                         :access_token_path => oauth_options['access_token_path'] }
-    consumer = OAuth::Consumer.new(oauth_options['consumer_key'], oauth_options['consumer_secret'], consumer_options)
+    # Get API settings
+    unless File.exists? @uconf['api_settings.yaml'].path
+      @uconf["api_settings.yaml"]["api_host"] = "http://api.telldus.com"
+      @uconf["api_settings.yaml"]["request_token_path"] = "/oauth/requestToken"
+      @uconf["api_settings.yaml"]["access_token_path"] = "/oauth/accessToken"
+      @uconf["api_settings.yaml"]["authorize_path"] = "/oauth/authorize"
+      # Get user input
+      puts "The first time you use the API you need to enter your Public and Private keys, they can be generated at:"
+      puts "\thttp://api.telldus.com/keys/index"
+      puts "\nPublic Key:"
+      public_key = $stdin.gets.strip
+      @uconf["api_settings.yaml"]["consumer_key"] = public_key
+      puts "\nPrivate Key:"
+      private_key = $stdin.gets.strip
+      @uconf["api_settings.yaml"]["consumer_secret"] = private_key
+      @uconf["api_settings.yaml"].save
+    end
+
+    # Setup OAuth
+    consumer_options = { :site => @uconf["api_settings.yaml"]['api_host'],
+                         :authorize_path => @uconf["api_settings.yaml"]['authorize_path'],
+                         :request_token_path => @uconf["api_settings.yaml"]['request_token_path'],
+                         :access_token_path => @uconf["api_settings.yaml"]['access_token_path'] }
+    consumer = OAuth::Consumer.new(@uconf["api_settings.yaml"]['consumer_key'], @uconf["api_settings.yaml"]['consumer_secret'], consumer_options)
 
     if File.exists? @uconf["api_token.yaml"].path
       @access_token = OAuth::AccessToken.new(consumer, @uconf["api_token.yaml"]["access_token"], @uconf["api_token.yaml"]["access_secret"])
